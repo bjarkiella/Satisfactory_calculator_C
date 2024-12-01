06fb32a46c221f512ae32f9909ad4e87540c73ee
bjarkiella <bjarkiella@gmail.com>
2024-12-01 22:25:19 +0100

    Starting with reading the excel file

---
+++ Utilities/ReadExcel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+
+namespace Satisfactory_calculator_C.Utilities
+
+
+
+
+
+{
+    public class ReadExcel(string pathin, string sheetname)
+    {
+        // Initializing
+        private string PathIn { get; } = pathin;
+        private string SheetName {get; } = sheetname;
+
+        // Private Method for reading the excel file and returning it
+
+
+        // Public Method that returns a specific sheet within the excel file
+
+    }
+}
